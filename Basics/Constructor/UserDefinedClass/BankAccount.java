998fa0594416413ba82eb537c17d8e4d85000163
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2022-07-26 04:20:14 +0600

    Create BankAccount.java

---
+++ Basics/Constructor/UserDefinedClass/BankAccount.java
@@ -0,0 +1,13 @@
+//The goal is to create multiple instance of object of a user-defined class
+
+public class BankAccount {
+    String name;
+    int AccountNum;
+    double balance;
+
+    BankAccount(String name, int AccountNum, double balance){
+        this.name = name;
+        this.AccountNum = AccountNum;
+        this.balance = balance;
+    }
+}
