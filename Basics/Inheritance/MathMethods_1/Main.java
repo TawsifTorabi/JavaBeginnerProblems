bc5b0ec2b94483cbe8c6ef6901610853b34a59c5
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2022-08-15 10:03:38 -0700

    Main Method

---
+++ Basics/Inheritance/MathMethods_1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        MathV2 math = new MathV2(3, 5, 25, 64, 34, 24);
+        math.power(2,3);
+        math.factorial();
+        math.fibo();
+        math.secondSmallest();
+    }
+}
