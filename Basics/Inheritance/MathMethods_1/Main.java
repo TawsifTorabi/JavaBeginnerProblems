cd109d2bae6e57b3f0ee592ba6f6dd92082bec2b
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2022-08-15 10:46:43 -0700

    Update Main.java

--- Basics/Inheritance/MathMethods_1/Main.java
+++ Basics/Inheritance/MathMethods_1/Main.java
@@ -1,9 +1,17 @@
-public class Main {
+public class MainMath {
     public static void main(String[] args) {
-        MathV2 math = new MathV2(3, 5, 25, 64, 34, 24);
+        MathV2 math = new MathV2(3, 5, 25, 64, 34, 3);
+
         math.power(2,3);
-        math.factorial();
+
+        System.out.println("Factorial Operation Result is : " + math.factorial());
+
+        math.printSeries();
+
         math.fibo();
+        System.out.println();
+
+        System.out.print("secondSmallest Operation Result is : ");
         math.secondSmallest();
     }
 }
