850cad5a64a141f4c0e05566d3a30779bfd7c812
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2022-08-15 15:52:54 -0700

    Update and rename SimpleProjects/DramaScriptSplitter/DramaScriptSplitter.java to Basics/Files/DramaScriptSplitter/DramaScriptSplitter.java

--- Basics/Files/DramaScriptSplitter/DramaScriptSplitter.java
+++ Basics/Files/DramaScriptSplitter/DramaScriptSplitter.java
@@ -1,16 +1,3 @@
-/*
-This is a Simple Script to Read a Drama Script from script.txt file and filter the Dialogues of two people into two different txt files.
-
-The sample script is,
-April: So now I'm crazy because I don't love you, right? Is that the point?
-Frank: No! Wrong! You're not crazy, and you do love me. That's the point, April.
-April: But I don't. I hate you. You were just some boy who made me laugh at a party once, and now I loathe the sight of you. In fact, if you come any closer, if you touch me or anything, I think I'll scream.
-Frank: Oh, come on, stop this April.
-April: What are you going to do now? Are you going to hit me? To show me how much you love me?
-Frank: Don't worry, I can't be bothered
-
-*/
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
